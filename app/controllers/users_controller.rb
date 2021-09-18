da94b8c8a6991a340ff7ef7ac337ee983793a18d
katsumasa.suwa <kt.suwa1023@gmail.com>
2021-09-18 09:31:41 +0000

    users_show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+    @pagy, @users = pagy(User.order(id: :desc), items: 25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+end
